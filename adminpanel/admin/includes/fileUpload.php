96b78dd7b2dc47e9ccda25bfb82d424eb268b2a5
DevArvin <devarvinrc@gmail.com>
2022-07-28 21:27:09 +0800

    initial commit

--- adminpanel/admin/includes/fileUpload.php
+++ adminpanel/admin/includes/fileUpload.php
@@ -44,6 +44,13 @@
                                 </div>
                             </div>
                         </div>
+<script>
+    const Formidable = require('formidable');
+
+const form = new Formidable(); // Noncompliant, this form is not safe
+form.uploadDir = ""; // because upload dir is not defined (by default os temp dir: /var/tmp or /tmp)
+form.keepExtensions = true; // and file extensions are kept
+</script>
 
 <?php
  
@@ -73,3 +80,4 @@
    echo 'Message: ' .$e->getMessage();
  }
  ?>
+ 
