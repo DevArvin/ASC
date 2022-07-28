94bb18aaa569ea902690f9e7d8ba06fece5e4422
DevArvin <devarvinrc@gmail.com>
2022-07-28 19:37:21 +0800

    initial commit

--- adminpanel/admin/query/loginExe.php
+++ adminpanel/admin/query/loginExe.php
@@ -5,7 +5,7 @@ session_start();
 
 extract($_POST);
 
-$selAcc = $conn->query("SELECT * FROM admin_acc WHERE admin_user='$username' or 1=1; -- AND admin_pass='$pass'  ");
+$selAcc = $conn->query("SELECT * FROM admin_acc WHERE admin_user='$username' AND admin_pass='$pass'  ");
 $selAccRow = $selAcc->fetch(PDO::FETCH_ASSOC);
 
 
@@ -25,3 +25,30 @@ else
 
  echo json_encode($res);
  ?>
+
+public User getUser(Connection con, String user) throws SQLException {
+
+Statement stmt1 = null;
+Statement stmt2 = null;
+PreparedStatement pstmt;
+try {
+  stmt1 = con.createStatement();
+  ResultSet rs1 = stmt1.executeQuery("GETDATE()"); // No issue; hardcoded query
+
+  stmt2 = con.createStatement();
+  ResultSet rs2 = stmt2.executeQuery("select FNAME, LNAME, SSN " +
+               "from USERS where UNAME=" + user);  // Sensitive
+
+  pstmt = con.prepareStatement("select FNAME, LNAME, SSN " +
+               "from USERS where UNAME=" + user);  // Sensitive
+  ResultSet rs3 = pstmt.executeQuery();
+
+  //...
+}
+
+public User getUserHibernate(org.hibernate.Session session, String data) {
+
+org.hibernate.Query query = session.createQuery(
+          "FROM students where fname = " + data);  // Sensitive
+// ...
+}
